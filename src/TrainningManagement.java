805fa6495b051f1ff694a4f723688bd2a19f9e68
seonjae <sunjae0423@naver.com>
2021-03-20 13:23:40 +0900

    added TrainningManagement.java

---
+++ src/TrainningManagement.java
@@ -0,0 +1,13 @@
+
+public class TrainningManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Trainning program");
+		System.out.println("2. Delete Trainning program");
+		System.out.println("3. Edit Trainning program");
+		System.out.println("4. View Trainning program");
+		System.out.println("5. Exit");
+
+	}
+
+}
